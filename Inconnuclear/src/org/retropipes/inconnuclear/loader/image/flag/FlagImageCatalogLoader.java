76bafa214e25cb900467a64be1905694dbbb3541
RetroPipes <eric.ahnell@puttysoftware.com>
2025-02-23 16:07:18 -0500

    Add flag image loading support.

---
+++ Inconnuclear/src/org/retropipes/inconnuclear/loader/image/flag/FlagImageCatalogLoader.java
@@ -0,0 +1,48 @@
+/*  Inconnuclear: A Dungeon-Diving RPG
+Copyleft (C) 2024-present RetroPipes
+Licensed under MIT. See the LICENSE file for details.
+
+All support is handled via the GitHub repository: https://github.com/retropipes/inconnuclear
+ */
+package org.retropipes.inconnuclear.loader.image.flag;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.retropipes.diane.Diane;
+import org.retropipes.diane.fileio.utility.ResourceStreamReader;
+import org.retropipes.inconnuclear.locale.Strings;
+
+class FlagImageCatalogLoader {
+    private static ArrayList<String> FILENAME_CACHE = null;
+    private static String CATALOG_PATH = "/asset/catalog/image/ui.catalog"; //$NON-NLS-1$
+
+    static String getFilename(final int index) {
+	if (FILENAME_CACHE == null) {
+	    try (final var rsr = new ResourceStreamReader(
+		    FlagImageCatalogLoader.class.getResourceAsStream(CATALOG_PATH))) {
+		// Fetch data
+		final var rawData = new ArrayList<String>();
+		var line = Strings.EMPTY;
+		while (line != null) {
+		    line = rsr.readString();
+		    if (line != null) {
+			rawData.add(line);
+		    }
+		}
+		FILENAME_CACHE = rawData;
+	    } catch (final IOException e) {
+		Diane.handleError(e);
+		return null;
+	    }
+	}
+	if (FILENAME_CACHE == null) {
+	    return null;
+	}
+	return FILENAME_CACHE.get(index);
+    }
+
+    private FlagImageCatalogLoader() {
+	// Do nothing
+    }
+}
