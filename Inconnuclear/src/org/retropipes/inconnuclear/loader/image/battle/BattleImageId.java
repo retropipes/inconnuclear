c08b18b66a7f25ce64cd05cfa0358e3eae872065
RetroPipes <eric.ahnell@puttysoftware.com>
2025-02-23 15:34:53 -0500

    Refresh battle image names.

--- Inconnuclear/src/org/retropipes/inconnuclear/loader/image/battle/BattleImageId.java
+++ Inconnuclear/src/org/retropipes/inconnuclear/loader/image/battle/BattleImageId.java
@@ -14,8 +14,39 @@ import org.retropipes.inconnuclear.locale.Strings;
 import org.retropipes.inconnuclear.locale.Untranslated;
 
 public enum BattleImageId implements DianeImageIndex {
-    TEAM0, TEAM0_NOACTIONS, TEAM1, TEAM1_NOACTIONS, TEAM2, TEAM2_NOACTIONS, TEAM3, TEAM3_NOACTIONS, TEAM4,
-    TEAM4_NOACTIONS, TEAM5, TEAM5_NOACTIONS, TEAM6, TEAM6_NOACTIONS, TEAM7, TEAM7_NOACTIONS, _NONE;
+    TEAM0,
+    TEAM0_NOACTIONS,
+    TEAM0_PARALYZED,
+    TEAM1,
+    TEAM1_NOACTIONS,
+    TEAM1_PARALYZED,
+    TEAM2,
+    TEAM2_NOACTIONS,
+    TEAM2_PARALYZED,
+    TEAM3,
+    TEAM3_NOACTIONS,
+    TEAM3_PARALYZED,
+    TEAM4,
+    TEAM4_NOACTIONS,
+    TEAM4_PARALYZED,
+    TEAM5,
+    TEAM5_NOACTIONS,
+    TEAM5_PARALYZED,
+    TEAM6,
+    TEAM6_NOACTIONS,
+    TEAM6_PARALYZED,
+    TEAM7,
+    TEAM7_NOACTIONS,
+    TEAM7_PARALYZED,
+    TEAM8,
+    TEAM8_NOACTIONS,
+    TEAM8_PARALYZED,
+    TEAM9,
+    TEAM9_NOACTIONS,
+    TEAM9_PARALYZED,
+    DAMAGE,
+    DEAD,
+    _NONE;
 
     @Override
     public String getName() {
